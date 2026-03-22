61d76529890aaeccea2a8bf4731073811f6166e9
xor1999 <benjamin.regazzoni@epfl.ch>
2026-03-22 17:07:03 +0100

    implementation of substraction technique with class SubComplements

--- techniques/subtraction.py
+++ techniques/subtraction.py
@@ -1,3 +1,4 @@
+from math import ceil
 from random import randint
 from models.technique import Technique, register
 
@@ -36,3 +37,30 @@ class Sub3Digit(Technique):
         if a < b:
             a, b = b, a
         return f"{a} - {b}", a - b
+
+
+@register
+class SubComplements(Technique):
+    name = "Subtraction by complements"
+    category = "subtraction"
+    description = "Subtract by rounding up, then adding back the complement"
+
+    def generate(self) -> tuple[str, int]:
+        a = randint(200, 999)
+        b = randint(100, a - 1)
+        # Ensure b doesn't end in 0 (complement would be trivial)
+        while b % 100 == 0:
+            b = randint(100, a - 1)
+        return f"{a} - {b}", a - b
+
+    def hint(self, problem: str) -> str:
+        a, b = (int(x) for x in problem.split(" - "))
+        # Round b up to next hundred
+        round_up = ceil(b / 100) * 100
+        complement = round_up - b
+        step1 = a - round_up
+        return (
+            f"Round {b} up to {round_up}, complement is {complement}:\n"
+            f"  {a} - {round_up} = {step1}\n"
+            f"  {step1} + {complement} = {step1 + complement}"
+        )
